92e712be3e59c907c78df006b9febef2c2d63ce3
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-14 12:40:19 +0400

    made hero section responsive

--- src/components/scenes/Hero.tsx
+++ src/components/scenes/Hero.tsx
@@ -4,20 +4,20 @@ import TextsDiv from '../textsDiv';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 min-h-[600px]">
+    <div className="py-16 flex flex-col items-center justify-center gap-12 min-h-[600px]">
       <div>
         <img src={logo} alt="Clipboard" />
       </div>
       <TextsDiv
         title="A history of everything you copy"
         desc="Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices."
-        titleSize="text-5xl"
-        descSize="text-2xl"
+        titleSize="text-4xl sm:text-5xl"
+        descSize="sm:text-2xl"
         descMaxWidth="max-w-[50rem]"
         textAligment="center"
         gapSize="mb-4"
       />
-      <div className="flex gap-4">
+      <div className="flex flex-col sm:flex-row gap-4">
         <Button text="Download for IOS" type="primary" />
         <Button text="Download for Mac" type="secondary" />
       </div>
