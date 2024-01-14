3dd79a8d2c96a1c2d58f00a4764db84b0f809ec9
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-14 14:19:27 +0400

    added Access section

---
+++ src/components/scenes/Access.tsx
@@ -0,0 +1,23 @@
+import TextsDiv from '../textsDiv';
+import tablet from '../../assets/images/image-devices.png';
+
+const Access = () => {
+  return (
+    <div className="flex flex-col items-center gap-10 py-10">
+      <TextsDiv
+        title="Access Clipboard anywhere"
+        desc="Whether you're on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks."
+        textAligment="center"
+        titleSize="text-xl sm:text-3xl"
+        descSize="text-sm"
+        descMaxWidth="max-w-md"
+        gapSize="mb-3"
+      />
+      <div>
+        <img src={tablet} alt="tablet-mobile" />
+      </div>
+    </div>
+  );
+};
+
+export default Access;
