2371fe8b404b66837ecbc2ed17559c9b07a54c5f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-14 20:18:56 +0400

    prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME',
+  base: '/clipboard-landing-page',
   plugins: [react()],
   resolve: {
     alias: [
