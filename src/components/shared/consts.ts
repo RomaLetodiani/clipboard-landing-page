8f3555806a2045a76c28b9c2be1b3b9ab79f2597
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-14 12:30:09 +0400

    prepared texts, images and etc

---
+++ src/components/shared/consts.ts
@@ -0,0 +1,79 @@
+export const snippets = [
+  {
+    id: 1,
+    title: 'Quick Search',
+    desc: 'Easily search your snippets by content, category, web address, application, and more.',
+  },
+  {
+    id: 2,
+    title: 'iCloud Sync',
+    desc: 'Instantly saves and syncs snippets across all your devices.',
+  },
+  {
+    id: 3,
+    title: 'Complete History',
+    desc: 'Retrieve any snippets from the first moment you started using the app.',
+  },
+];
+
+export const workFlow = [
+  {
+    id: 1,
+    icon: 'assets/images/icon-blacklist.svg',
+    title: 'Create blacklists',
+    desc: 'Ensure sensitive information never makes its way to your clipboard by excluding certain sources',
+  },
+  {
+    id: 2,
+    icon: 'assets/images/icon-text.svg',
+    title: 'Plain text snippets',
+    desc: 'Remove unwanted formatting from copied text for a consistent look',
+  },
+  {
+    id: 3,
+    icon: 'assets/images/icon-preview.svg',
+    title: 'Sneak preview',
+    desc: 'Quick preview of all snippets on your Clipboard for easy access',
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    logo: 'assets/images/logo-google.svg',
+  },
+  {
+    id: 2,
+    logo: 'assets/images/logo-ibm.svg',
+  },
+  {
+    id: 3,
+    logo: 'assets/images/logo-microsoft.svg',
+  },
+  {
+    id: 4,
+    logo: 'assets/images/logo-hp.svg',
+  },
+  {
+    id: 5,
+    logo: 'assets/images/logo-vector-graphics.svg',
+  },
+];
+
+export const socials = [
+  {
+    id: 1,
+    icon: 'assets/images/icon-facebook.svg',
+    link: 'https://www.facebook.com/LetodianiRoma/',
+  },
+  {
+    id: 2,
+    icon: 'assets/images/icon-twitter.svg',
+    link: 'https://twitter.com/l3todianiroma',
+  },
+  {
+    id: 3,
+    icon: 'assets/images/icon-instagram.svg',
+    link: 'https://www.instagram.com/roma.letodiani',
+  },
+];
