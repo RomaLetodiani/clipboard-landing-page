be7c9cfeb8cd76e4092e784f855ed92e0cd49be1
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-14 12:31:26 +0400

    created component for texts pairs (title, description)

---
+++ src/components/TextsDiv.tsx
@@ -0,0 +1,38 @@
+type Props = {
+  title: string;
+  desc: string;
+  textAligment?: string;
+  titleSize: string;
+  descSize: string;
+  descMaxWidth?: string;
+  gapSize?: string;
+};
+
+const TextsDiv = ({
+  title,
+  desc,
+  textAligment,
+  titleSize,
+  descSize,
+  descMaxWidth,
+  gapSize,
+}: Props) => {
+  return (
+    <div
+      className={`${
+        textAligment === 'center'
+          ? 'text-center'
+          : textAligment === 'right'
+          ? 'text-right'
+          : 'text-left'
+      }`}
+    >
+      <p className={`${titleSize} ${gapSize} font-semibold text-darkGreyBlue`}>
+        {title}
+      </p>
+      <p className={`${descSize} ${descMaxWidth} text-GrayishBlue`}>{desc}</p>
+    </div>
+  );
+};
+
+export default TextsDiv;
